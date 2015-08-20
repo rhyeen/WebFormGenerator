7771b89edff6d43abcaca85c5dd90c497597b2f3
Ryan Saunders <contact.ryan.saunders@gmail.com>
2015-08-20 11:24:52 -0600

    Uploading to GitHub

---
+++ php/db/config.php
@@ -0,0 +1,7 @@
+<?php
+// we keep this in a separate file so that we can secure this information away from the user if needed
+$server_name  = 'localhost';
+$db_user_name = 'realiue2_site';
+$db_password  = 'ret2fli9';
+$db_name      = 'realiue2_realitypd';
+?>
